f3ece924a6c3426e70a5c2164d39f96af58f56bd
iTao <itao_1314@icloud.com>
2022-01-21 14:38:48 +0800

    存取历史记录数据

--- HIITFit/Models/HistoryStore.swift
+++ HIITFit/Models/HistoryStore.swift
@@ -67,10 +67,41 @@ class HistoryStore: ObservableObject {
         } else {
             exerciseDays.insert(ExerciseDay(date: today, exercises: [exerciseName]), at: 0)
         }
+
+        do {
+            try save()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
     }
 
     func load() throws {
-        throw FileError.loadFailure
+        guard let dataURL = getURL() else { throw FileError.urlFailure }
+        guard let data = try? Data(contentsOf: dataURL) else { return }
+        let plistData = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+        let convertedPlistData = plistData as? [[Any]] ?? []
+        exerciseDays = convertedPlistData.map({
+            ExerciseDay(date: $0[1] as? Date ?? Date(), exercises: $0[2] as? [String] ?? [])
+        })
+
+    }
+
+    func save() throws {
+        guard let dataURL = getURL() else { throw FileError.urlFailure }
+        let plistData = exerciseDays.map {
+            [$0.id.uuidString, $0.date, $0.exercises]
+        }
+        do {
+            let data = try PropertyListSerialization.data(fromPropertyList: plistData, format: .binary, options: .zero)
+            try data.write(to: dataURL, options: .atomic)
+        } catch {
+            throw FileError.saveFailure
+        }
+    }
+
+    func getURL() -> URL? {
+        guard let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return documentURL.appendingPathComponent("history.plist")
     }
 }
 
